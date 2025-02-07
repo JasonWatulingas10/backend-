0d0f190007fa2869e149dbdea979a3c8e02398d5
JasonWatulingas10 <s22110562@student.unklab.ac.id>
2025-02-07 19:11:26 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+const http = require('http');  
+const helloWorld = ('./helloWorld')
+const moment = require('moment')
+
+
+
+const server = http.createServer((req, res) => {
+    
+    // res.write(hello)
+    // res.write(greetings())
+    const url = req.url;
+    if(url === '/'){
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'text/json')
+        res.write(JSON.stringify({
+            status: 'success',
+            message: 'Users',
+            date: moment().format('MMMM Do YYYY, h:mm:ss a')
+        }))
+    }
+    else if(url === '/about'){
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'text/json')
+        res.write(JSON.stringify({
+            status: 'success',
+            message: 'Post',
+            date: moment().format('MMMM Do YYYY, h:mm:ss a')
+        }))
+    } else {
+        res.statusCode = 404
+        res.setHeader('Content-Type', 'text/json')
+        res.write(JSON.stringify({
+            status: 'not found',
+            message: 'Route tidak ditemukan',
+            date: moment().format('MMMM Do YYYY, h:mm:ss a')
+        }))
+    }
+    res.end()
+})
+
+const hostname = "127.0.0.1"
+const port = 3000
+server.listen(port, hostname, () => console.log(`Server running at http://${hostname}:${port}`))
